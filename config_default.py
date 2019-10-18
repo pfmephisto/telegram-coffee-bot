f31765b32f08b6f55a017d4298bf00aa29bfa06d
Povl Filip Sonne-Frederiksen <psf@aarch.dk>
2019-10-18 12:46:03 +0200

    don't overwriting enviroment variables if CI

--- config_default.py
+++ config_default.py
@@ -3,19 +3,21 @@
 # This program is dedicated to the public domain under the GNU GPLv3 license.
 import os
 
-#Telegram
-os.environ['sysadmin'] = 'username' #User handle of main admin
 devs = [] #The developers. Will be notified if error ocur on the bot
 admins = [] # Users thet shoud have andinistive rights
 
-os.environ['tokenTelegram'] = 'username' #Telegram api token
-os.environ['Telegram_db'] = 'username' # Telegram DB for persistance
+if (CI == False):
+    #Telegram
+    os.environ['sysadmin'] = 'username' #User handle of main admin
 
-os.environ['Reddit_secret'] = 'username' #Rddit secret
-os.environ['Reddit_id'] = 'username' #Reddit ID
-os.environ['Reddit_user'] = 'username' #Reddit user name
-os.environ['Reddit_password'] = 'username' #Reddit password
-os.environ['Reddit_user_agent'] = 'username' #Reddit bot useragent
+    os.environ['tokenTelegram'] = 'username' #Telegram api token
+    os.environ['Telegram_db'] = 'username' # Telegram DB for persistance
 
-os.environ['brewingPin'] = 0 # Singal pin on rassberryPi
-os.environ['user_agent'] = 0 # Signal pin on rassberryPi
+    os.environ['Reddit_secret'] = 'username' #Rddit secret
+    os.environ['Reddit_id'] = 'username' #Reddit ID
+    os.environ['Reddit_user'] = 'username' #Reddit user name
+    os.environ['Reddit_password'] = 'username' #Reddit password
+    os.environ['Reddit_user_agent'] = 'username' #Reddit bot useragent
+
+    os.environ['brewingPin'] = 0 # Singal pin on rassberryPi
+    os.environ['user_agent'] = 0 # Signal pin on rassberryPi
